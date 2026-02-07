93a35a04bed50adb1e3a84173e80cb6cd42c5666
fandyrio <fandyrio@gmail.com>
2026-02-07 14:27:58 +0700

    add info

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -927,7 +927,7 @@ use Symfony\Component\CssSelector\Node\HashNode;
                                         // echo $variable_penilai;die();
                                         // echo $data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]['is_plt'].", ";
                                         // Log::error($s.", ".$variable_penilai." ".${"pointer_{$variable_penilai}"}." ".$variable_jabatan_peserta);
-                                        var_dump($data_peserta[$s]);
+                                        echo "data_peserta_".$s."_".$variable_penilai."_"."pointer_".$variable_penilai;
                                         if($data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]['is_plt'] === 
                                         "true" && (int)$data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]['id_pegawai'] === 0){
                                             $id_pegawai_penilai=null; 
