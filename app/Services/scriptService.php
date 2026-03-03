d346b6a5e65f90a703a50f36a7108132d8a9985e
fandyrio <fandyrio@gmail.com>
2026-03-03 19:35:55 +0700

    skrip isi pertanyaan fix id

--- app/Services/scriptService.php
+++ app/Services/scriptService.php
@@ -45,7 +45,7 @@ use App\Models\Tref_zonasi;
                     $nilai_tmp=$nilai*$bobot["bobot_{$list_nilai['id_pertanyaan']}"] / 100;
                     $current_nilai += $nilai_tmp;
                 }
-                var_dump($list_peserta_zonasi);
+                var_dump($list_peserta_zonasi['id']);
                 //Trans_peserta_zonasi::where('id', $list_peserta_zonasi['id'])->update(['nilai', $current_nilai]);
                 //clone $get_nilai->update(['locked'=>true, 'updated_at'=>date('Y-m-d H:i:s')]);
             }
