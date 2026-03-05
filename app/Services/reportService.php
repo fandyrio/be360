67ed857d84e66543fd93519652d12649154c53fd
fandyrio <fandyrio@gmail.com>
2026-03-05 10:29:01 +0700

    Fix report kolom pegawai belum menjumlahkan orangnya

--- app/Services/reportService.php
+++ app/Services/reportService.php
@@ -19,10 +19,10 @@ use Symfony\Component\HttpKernel\HttpCache\Store;
             $id_periode=$get_zonasi['id_periode'];
             if((int)$id_periode_ctrl === $id_periode){
                 /** @var \Illuminate\Support\Collection $get_jabatan_periode */
-                if($refesh === true){
+                // if($refesh === true){
                     Cache::store('redis')->forget("jabatan_periode_{$id_periode}");
                     Cache::store('redis')->forget("report_periode_zs_satker_{$id_periode}_{$id_zonasi_satker}_{$id_zonasi}");
-                }
+                // }
                 $get_jabatan_periode=Cache::store('redis')->remember("jabatan_periode_{$id_periode}", 3600*24*24, function() use($id_periode){
                     return DB::table("tref_jabatan_peserta as tjp")
                                 ->whereRaw("id IN (SELECT DISTINCT(id_jabatan_peserta) from tref_mapping_jabatan as tmj 
@@ -37,7 +37,7 @@ use Symfony\Component\HttpKernel\HttpCache\Store;
                         CASE 
                             WHEN toe.id_kelompok_jabatan = {$row->id_kelompok_jabatan} 
                                 THEN 1
-                            WHEN toe.id_jabatan_gabungan = {$row->id_kelompok_jabatan}
+                            WHEN tjp.id_jabatan_gabungan = {$row->id_kelompok_jabatan}
                                 THEN 1 
                             ELSE 0 
                         END) AS `{$row->jabatan}`";
