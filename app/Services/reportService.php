34e24c087413e00b641336daf1f9bb90d63347d9
fandyrio <fandyrio@gmail.com>
2026-03-06 13:19:06 +0700

    fix report penilaian personal

--- app/Services/reportService.php
+++ app/Services/reportService.php
@@ -73,8 +73,10 @@ use Vinkla\Hashids\Facades\Hashids;
                 $data_report=Cache::store('redis')->remember("report_periode_zs_satker_{$id_periode}_{$id_zonasi_satker}_{$id_zonasi}", 3600*24*365, function() use($sql, $id_zonasi, $id_zonasi_satker){
                     $data = DB::select($sql, [$id_zonasi, $id_zonasi_satker]);
                     foreach($data as $list_data){
-                        $list_data->token_o=Crypt::encrypt($list_data->id_observee_peserta);
-                        $list_data->token_p=Hashids::encode($list_data->id_pegawai_peserta);
+                        $token_o=Crypt::encrypt($list_data->id_observee_peserta);
+                        $token_p=Hashids::encode($list_data->id_pegawai_peserta);
+                        $endpoint=$token_o."063atam".$token_p;
+                        $list_data->endpoint=$endpoint;
                         unset($list_data->id_observee_peserta, $list_data->id_pegawai_peserta);
                     }
                     return $data;
