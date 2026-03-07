bab682df3af59fd41ff4b0bd96199a12b04de1fc
fandyrio <fandyrio@gmail.com>
2026-03-07 20:25:50 +0700

    setting flag untuk kirim badilum

--- app/Services/reportService.php
+++ app/Services/reportService.php
@@ -5,6 +5,7 @@ use App\Models\Trans_nilai_peserta_zonasi;
 use App\Models\Trans_observee;
 use App\Models\Trans_peserta_zonasi;
 use App\Models\Tref_jabatan_peserta;
+use App\Models\Zonasi_satker;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +19,7 @@ use Vinkla\Hashids\Facades\Hashids;
             $this->penilaianService=$penilaian_service;
         }   
         public function generateDataReport($id_zonasi_satker, $id_zonasi, $id_periode_ctrl, $refesh){
-            $data=[];
+            $data_report=[];
             $msg="";
             $status=false;
             $get_zonasi=$this->penilaianService->getZonasi($id_zonasi_satker);
@@ -29,61 +30,69 @@ use Vinkla\Hashids\Facades\Hashids;
                     Cache::store('redis')->forget("jabatan_periode_{$id_periode}");
                     Cache::store('redis')->forget("report_periode_zs_satker_{$id_periode}_{$id_zonasi_satker}_{$id_zonasi}");
                 // }
-                $get_jabatan_periode=Cache::store('redis')->remember("jabatan_periode_{$id_periode}", 3600*24*24, function() use($id_periode){
-                    return DB::table("tref_jabatan_peserta as tjp")
-                                ->whereRaw("id IN (SELECT DISTINCT(id_jabatan_peserta) from tref_mapping_jabatan as tmj 
-                                            JOIN trans_mapping_jabatan_periode as tmjp on tmjp.id_mapping_jabatan = tmj.id where tmjp.id_periode = {$id_periode})"
-                                )
-                                ->get();
+                
+                $get_zonasi_satker = Zonasi_satker::where("IdZonaSatker", $id_zonasi_satker)->first();
 
-                });
+                //check apakai zonasi satker nya ada dan sudah kirim penilaian nya ke badilum
+                if(!is_null($get_zonasi_satker) && (int)$get_zonasi_satker['kirim_penilaian'] === 1){
+                    $get_jabatan_periode=Cache::store('redis')->remember("jabatan_periode_{$id_periode}", 3600*24*24, function() use($id_periode){
+                        return DB::table("tref_jabatan_peserta as tjp")
+                                    ->whereRaw("id IN (SELECT DISTINCT(id_jabatan_peserta) from tref_mapping_jabatan as tmj 
+                                                JOIN trans_mapping_jabatan_periode as tmjp on tmjp.id_mapping_jabatan = tmj.id where tmjp.id_periode = {$id_periode})"
+                                    )
+                                    ->get();
 
-                $jabatan_cols=$get_jabatan_periode->map(function($row){
-                    return "SUM(
-                        CASE 
-                            WHEN toe.id_kelompok_jabatan = {$row->id_kelompok_jabatan} 
-                                THEN 1
-                            WHEN tjp.id_jabatan_gabungan is not null and tjp.id_jabatan_gabungan = {$row->id}
-                                THEN 1 
-                            ELSE 0 
-                        END) AS `{$row->jabatan}`";
-                })->implode(', ');
+                    });
 
-                $sql="
-                    SELECT toe2.IdObservee as id_observee_peserta, tp.id as id_pegawai_peserta, tp.nama_pegawai, 
-                        CASE
-                            when toe2.NamaJabatan = 'Panitera Muda'
-                                THEN 
-                                    toe2.bagian
-                                else
-                                    toe2.NamaJabatan
-                            end 
-                            as jabatan, 
-                        $jabatan_cols, MAX(toe2.total_nilai) as nilai
-                    from trans_peserta_zonasi as tpz
-                    JOIN trans_observee as toe on toe.IdObservee = tpz.id_pegawai_penilai
-                    JOIN tref_jabatan_peserta as tjp on tjp.id_kelompok_jabatan = toe.id_kelompok_jabatan
-                    JOIN trans_observee as toe2 on toe2.IdObservee = tpz.id_pegawai_peserta
-                    JOIN tref_pegawai as tp on tp.id_pegawai = toe2.IdPegawai
-                    where tpz.id_zonasi = ? and id_zona_satker = ?
-                    group by tp.nama_pegawai, toe2.NamaJabatan, toe2.bagian,  toe2.IdObservee, tp.id
-                    order by nilai desc
-                ";
+                    $jabatan_cols=$get_jabatan_periode->map(function($row){
+                        return "SUM(
+                            CASE 
+                                WHEN toe.id_kelompok_jabatan = {$row->id_kelompok_jabatan} 
+                                    THEN 1
+                                WHEN tjp.id_jabatan_gabungan is not null and tjp.id_jabatan_gabungan = {$row->id}
+                                    THEN 1 
+                                ELSE 0 
+                            END) AS `{$row->jabatan}`";
+                    })->implode(', ');
 
-                $data_report=Cache::store('redis')->remember("report_periode_zs_satker_{$id_periode}_{$id_zonasi_satker}_{$id_zonasi}", 3600*24*365, function() use($sql, $id_zonasi, $id_zonasi_satker, $id_periode){
-                    $data = DB::select($sql, [$id_zonasi, $id_zonasi_satker]);
-                    foreach($data as $list_data){
-                        $token_o=Crypt::encrypt($list_data->id_observee_peserta);
-                        $token_p=encodeInt($list_data->id_pegawai_peserta);
-                        $token_periode=encodeInt($id_periode);
-                        $endpoint=$token_o."063atam".$token_p."063atam".$token_periode;
-                        $list_data->endpoint=$endpoint;
-                        unset($list_data->id_observee_peserta, $list_data->id_pegawai_peserta);
-                    }
-                    return $data;
-                });
-                $status=true;
-                $msg="Data ditemukan";
+                    $sql="
+                        SELECT toe2.IdObservee as id_observee_peserta, tp.id as id_pegawai_peserta, tp.nama_pegawai, 
+                            CASE
+                                when toe2.NamaJabatan = 'Panitera Muda'
+                                    THEN 
+                                        toe2.bagian
+                                    else
+                                        toe2.NamaJabatan
+                                end 
+                                as jabatan, 
+                            $jabatan_cols, MAX(toe2.total_nilai) as nilai
+                        from trans_peserta_zonasi as tpz
+                        JOIN trans_observee as toe on toe.IdObservee = tpz.id_pegawai_penilai
+                        JOIN tref_jabatan_peserta as tjp on tjp.id_kelompok_jabatan = toe.id_kelompok_jabatan
+                        JOIN trans_observee as toe2 on toe2.IdObservee = tpz.id_pegawai_peserta
+                        JOIN tref_pegawai as tp on tp.id_pegawai = toe2.IdPegawai
+                        where tpz.id_zonasi = ? and id_zona_satker = ?
+                        group by tp.nama_pegawai, toe2.NamaJabatan, toe2.bagian,  toe2.IdObservee, tp.id
+                        order by nilai desc
+                    ";
+
+                    $data_report=Cache::store('redis')->remember("report_periode_zs_satker_{$id_periode}_{$id_zonasi_satker}_{$id_zonasi}", 3600*24*365, function() use($sql, $id_zonasi, $id_zonasi_satker, $id_periode){
+                        $data = DB::select($sql, [$id_zonasi, $id_zonasi_satker]);
+                        foreach($data as $list_data){
+                            $token_o=Crypt::encrypt($list_data->id_observee_peserta);
+                            $token_p=encodeInt($list_data->id_pegawai_peserta);
+                            $token_periode=encodeInt($id_periode);
+                            $endpoint=$token_o."063atam".$token_p."063atam".$token_periode;
+                            $list_data->endpoint=$endpoint;
+                            unset($list_data->id_observee_peserta, $list_data->id_pegawai_peserta);
+                        }
+                        return $data;
+                    });
+                    $status=true;
+                    $msg="Data ditemukan";
+                }else{
+                    $msg="Satker ini belum mengirimkan hasil penilaian";
+                }
             }else{
                 $msg="Data tidak konsisten";
             }
