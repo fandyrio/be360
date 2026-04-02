976032adb3b541d1e03a0df28a5bba1bb38aacd5
Fandy <fandyrio@gmail.com>
2026-04-02 10:39:09 +0700

    fix

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -1076,7 +1076,7 @@ use Symfony\Component\CssSelector\Node\HashNode;
                                                 }
                                             }
                                         }
-                                        echo $data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]."<br />";
+                                        var_dump($data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]);echo "<br />";
                                         if($data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]['is_plt'] === 
                                         "true" && (int)$data_peserta[$s][$variable_penilai][${"pointer_{$variable_penilai}"}]['id_pegawai'] === 0){
                                             $id_pegawai_penilai=null; 
