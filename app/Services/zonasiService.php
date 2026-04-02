c1121f1620b31254840bbf252c995f5f55f4448a
Fandy <fandyrio@gmail.com>
2026-04-02 10:32:13 +0700

    fix

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -723,6 +723,7 @@ use Symfony\Component\CssSelector\Node\HashNode;
                             ${"index_{$variable}"}=0;
                             ${"counter_{$variable}"}=0;
                         }else{
+                            echo "sudah ada ".$variable;
                             ${"pointer_{$variable}"}+=1;
                             ${"index_{$variable}"}+=1;
                             ${"counter_{$variable}"}+=1;
