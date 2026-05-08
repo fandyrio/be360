3cd557c740bedc620bc287f5162c43c07742ff71
Fandy <fandyrio@gmail.com>
2026-05-08 22:29:08 +0700

    hardcode wa testing ke nip agif

--- app/Helpers/checkUser.php
+++ app/Helpers/checkUser.php
@@ -41,10 +41,10 @@ use Vinkla\Hashids\Facades\Hashids;
             }else if($env === "testing" || $env === "development"){
                 $data=[
                     'token'=>config('services.WA_MA.token'),
-                    'nip'=>"199306242019031004",
+                    'nip'=>"199702182020121006",
                     'message'=>$msg_wa,
-                    'phoneNumber'=>"085880037948",
-                    'name'=>"Fandy Juniario Simorangkir",
+                    'phoneNumber'=>"087822507250",
+                    'name'=>"Agief Muftahid",
                     'serviceMode'=>config('services.WA_MA.serviceMode')
                 ];
             }
