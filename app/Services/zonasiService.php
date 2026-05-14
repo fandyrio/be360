17fd3f36b4898fed7727f567e360f4ce23915b4a
Fandy <fandyrio@gmail.com>
2026-05-14 16:34:55 +0700

    debug: query on log

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -967,7 +967,7 @@ use Symfony\Component\CssSelector\Node\HashNode;
                 $index_satker++;
                 $id_satker_before=$list_satker['IdSatker'];
             }
-            Log::info("Data Peserta: ", $getPeserta);
+            Log::info("Data Peserta: ", $getPeserta->toArray());
             // var_dump($variable_jabatan_peserta_arr);exit();
             // ketua_pengadilan
             $data=[];
