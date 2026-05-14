afd3f5dccbf32dc93bf9dc77883ff7fec1b1a651
Fandy <fandyrio@gmail.com>
2026-05-14 19:29:02 +0700

    debug

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -756,8 +756,8 @@ use Symfony\Component\CssSelector\Node\HashNode;
                                 $include="false";
                             }   
                             if($include === "true"){
-                                if($list_peserta['nama_pegawai'] === "Emma Herminawaty Simanjuntak"){
-                                    Log::warning("Emma Herminawaty Simanjuntak Masuk");
+                                if($list_peserta['nama_pegawai'] === "Mifta Namira"){
+                                    Log::warning("Mifta Namira Masuk");
                                 }
                                 //set new variable bila 
                                 $data_peserta[$index_satker][$variable][${"index_{$variable}"}]['nama']=$list_peserta['nama_pegawai'];
