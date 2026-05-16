adf80c7e2c7b701af676b907a7af4e9054e4fd0a
Fandy <fandyrio@gmail.com>
2026-05-16 18:24:09 +0700

    fix route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Cache;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        // web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
