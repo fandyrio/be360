6a844dd3da2007550d2c98316029d4c54627fb1c
Fandy <fandyrio@gmail.com>
2026-08-02 21:11:30 +0700

    fix

--- app/Services/zonasiService.php
+++ app/Services/zonasiService.php
@@ -1638,7 +1638,7 @@ use Symfony\Component\CssSelector\Node\HashNode;
                                         $join->on("tmh.id_zonasi_satker", "=", "tzs.IdZonaSatker")
                                             ->where("status", true);
                                     })
-                                    ->select("v_satker.IdSatker", "v_satker.NamaSatker", DB::raw("COUNT(*) as jumlah"))
+                                    ->select("v_satker.IdSatker", "v_satker.NamaSatker", DB::raw("COUNT(DISTINCT nama_majelis) as jumlah"))
                                     ->where("tz.IdZona", $id_zonasi)
                                     ->groupBy("v_satker.NamaSatker")
                                     ->orderBy("v_satker.IdSatker")
