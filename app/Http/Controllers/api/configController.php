b6c795b0cf257264fdc855762c64bd05d097b9b3
Fandy <fandyrio@gmail.com>
2026-08-06 10:22:06 +0700

    fix

--- app/Http/Controllers/api/configController.php
+++ app/Http/Controllers/api/configController.php
@@ -338,7 +338,7 @@ class configController extends Controller
                 if($request->is_pt === true){
                     $is_pt = true;
                 }
-                $update_jabatan=$this->configService->updateKelompokJabatan($jabatan_gabungan, $token_jabatan[0], $request->jabatan, $request->active, $token_category[0], $is_pn, $is_pt);
+                $update_jabatan=$this->configService->updateKelompokJabatan($jabatan_gabungan, $token_jabatan[0], $request->jabatan, $request->active, $token_category[0], true, true);
                 $status=$update_jabatan['status'];
                 $msg=$update_jabatan['msg'];
             }catch(\Exception $e){
