b8ad6bab4bf4f0aeac102df3e77940fd19f7278a
Fandy <fandyrio@gmail.com>
2026-08-06 10:29:31 +0700

    fix

--- app/Models/Tref_jabatan_peserta.php
+++ app/Models/Tref_jabatan_peserta.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Tref_jabatan_peserta extends Model
 {
     protected $table="tref_jabatan_peserta";
-    protected $fillable=['id_kelompok_jabatan', 'jabatan', 'active'];
+    protected $fillable=['id_kelompok_jabatan', 'jabatan', 'active', 'pn', 'pt'];
 }
